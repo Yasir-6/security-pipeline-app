2390a9eafa1f8a9e32ca8fabf23f6361f1e14d2d
Yasir-6 <myasir24206@gmail.com>
2025-07-10 15:14:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, Secure App!'));
+app.get('/', (req, res) => res.send('Hello, Secure App It is correctly setup!'));
 app.listen(3000, () => console.log('Server on port 3000'));
