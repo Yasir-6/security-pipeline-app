ad2e087fb8f28937d36526a396df01cb4f249584
Yasir-6 <myasir24206@gmail.com>
2025-07-10 15:37:11 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello, Secure App It is correctly setup!'));
+app.get('/', (req, res) => res.send('Hello, Secure App It is correctly setup and now pipleine is correctly setup!'));
 app.listen(3000, () => console.log('Server on port 3000'));
